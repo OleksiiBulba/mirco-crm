ea2507b0f158271f4412eaba29b86a17bcb20b62
Oleksii Bulba <oleksii_bulba@epam.com>
2022-12-24 23:36:26 +0400

    Feature: React JS
    - Added simple login;
    
    Signed-off-by: Oleksii Bulba <oleksii_bulba@epam.com>

--- assets/main.jsx
+++ assets/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from "./components/App/App";
 
 const container = document.getElementById('app');
 const root = createRoot(container);
